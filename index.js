eaafb7884ff8b90c10d7206bed8d786fd70d3288
Anikesh348 <44468436+Anikesh348@users.noreply.github.com>
2020-05-12 14:02:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function myFunction() {
   if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 wins!!";
   } else if (randomNumber1 === randomNumber2) {
-    document.querySelector("h1").innerHTML = "Draw";
+    document.querySelector("h1").innerHTML = "Its a tiw";
   } else {
     document.querySelector("h1").innerHTML = "Player 2 wins!!";
   }
